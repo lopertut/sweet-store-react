5a6c0045b4e60d7ff17edf446cacf09ecf7a0de1
lopertut <artur.matvejev12345@gmail.com>
2025-10-23 23:00:11 +0300

    made api for users

--- backend/db.php
+++ backend/db.php
@@ -7,7 +7,6 @@ $dbname = "sweet-store";
 try {
 	$pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	echo "Connected successfully";
 } catch (PDOException $e) {
 	echo "Connection failed: " . $e->getMessage();
 }
---
+++ backend/users.php
@@ -0,0 +1,46 @@
+<?php
+header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Max-Age: 86400");
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
+header("Access-Control-Allow-Methods: PUT, POST, GET, DELETE");
+
+include 'db.php';
+
+$method = $_SERVER['REQUEST_METHOD'];
+$input = json_decode(file_get_contents('php://input', true));
+
+switch ($method) {
+	case 'GET':
+		handleGet($pdo);
+		break;
+	case 'POST':
+		handlePost($pdo, $input);
+		break;
+	//	case 'PUT':
+	//		handlePut($pdo, $input);
+	//		break;
+	//	case 'DELETE':
+	//		handleDelete($pdo, $input);
+	//		break;
+	default:
+		echo json_encode(['message' => 'Invalid request method']);
+		break;
+}
+
+function handleGet($pdo)
+{
+	$sql = "SELECT * FROM users";
+	$stmt = $pdo->prepare($sql);
+	$stmt->execute();
+	$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	echo json_encode($result);
+}
+
+function handlePost($pdo, $input)
+{
+	$sql = "INSERT INTO users(username, password) VALUES(:username, :password)";
+	$stmt = $pdo->prepare($sql);
+	$stmt->execute(['username' => $input->username, 'password' => $input->password]);
+	echo json_encode(['message' => 'User created']);
+}
