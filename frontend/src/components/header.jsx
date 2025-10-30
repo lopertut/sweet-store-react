b542b4f58922f31f8210e69f956d8098c9dc8cd2
lopertut <artur.matvejev12345@gmail.com>
2025-10-30 17:00:32 +0200

    checking user login status

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,7 +1,22 @@
 import { Link } from "react-router"
 import '../css/styles.css'
+import { useEffect } from "react";
 
 export default function Header() {
+	useEffect(() => {
+		fetch("http://localhost:8000/check-session.php", {
+			credentials: "include"
+		})
+			.then(response => response.json())
+			.then(data => {
+				if (data.loggedIn) {
+					console.log("user logged in: ", data.userId);
+				} else {
+					console.log("user is not logged in");
+				}
+			})
+	});
+
 	return (
 		<header>
 			<Link to="/"><img className="logo" src="src/assets/images/logo.png" alt="Logo Image" /></Link>
