a82878b1fed000c3dcee6a736a47be5ed61beaf2
lopertut <artur.matvejev12345@gmail.com>
2025-10-30 17:01:39 +0200

    product adding to cart

--- frontend/src/pages/store.jsx
+++ frontend/src/pages/store.jsx
@@ -4,16 +4,39 @@ import { useEffect, useState } from 'react';
 
 export default function Store() {
 	const [products, setProduct] = useState([]);
+	const [cartId, setCartid] = useState();
 
 	useEffect(() => {
 		fetch("http://localhost:8000/sweets.php")
-			.then((response) => response.json())
+			.then(response => response.json())
 			.then(data => {
 				console.log(data);
 				setProduct(data)
 			})
 			.catch(error => console.log(error))
 	}, []);
+
+	useEffect(() => {
+		fetch("http://localhost:8000/carts.php", {
+			credentials: "include"
+		})
+			.then(response => response.json())
+			.then(data => {
+				console.log(data.cartId);
+				setCartid(data.cartId)
+			})
+			.catch(error => console.log(error))
+	})
+
+	function addCartItem(cartId, sweetId) {
+		let called = false;
+		fetch("http://localhost:8000/carts.php", {
+			method: "POST",
+			body: JSON.stringify({ cartId: cartId, sweetId: sweetId, quantity: 1 })
+		})
+			.catch(error => console.log(error))
+		called = true;
+	}
 
 	return (
 		<>
@@ -24,7 +47,7 @@ export default function Store() {
 						<img className="product-image" src={product.imageUrl} alt="Product Image" />
 						<p>{product.name}</p>
 						<p>{product.price}</p>
-						<a href="#"><img className="add-button" src="src/assets/images/add-button.png" alt="Add Button" /></a>
+						<a href="#"><img className="add-button" onClick={() => addCartItem(cartId, product.id)} src="src/assets/images/add-button.png" alt="Add Button" /></a>
 					</div>
 				))}
 			</main>
