f86f8fedb30b50f380e1b85e79842a1019992ac0
MihhailKakourov <example@example.com>
2025-11-04 09:45:18 +0200

    put dns into variable

--- backend/db.php
+++ backend/db.php
@@ -3,9 +3,10 @@ $host = "localhost";
 $user = "root";
 $password = "";
 $dbname = "sweet-store";
+$dsn = "mysql:host=$host;dbname=$dbname";
 
 try {
-	$pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+	$pdo = new PDO($dsn, $user, $password);
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
 	echo "Connection failed: " . $e->getMessage();
